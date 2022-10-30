5c4a7ec7eb053de92adba78f142f91209294de4e
adamrill <adampurbalingga@gmail.com>
2022-10-30 17:21:59 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos sunt voluptatum repudiandae reprehenderit laudantium cum. Iusto, aut ea amet dolorem vel, aperiam ab rem quasi eaque laudantium suscipit unde recusandae.</p>
+    <ul>
+        <li>adam</li>
+        <li>fariz</li>
+    </ul>
 </body>
 </html>
